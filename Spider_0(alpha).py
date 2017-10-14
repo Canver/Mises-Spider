b73f51eda1e91cdaf2ffa57d173f2b342b5bb1fa
Canver <32225741+Canver@users.noreply.github.com>
2017-10-14 19:07:09 +0800

    Update Spider_0(alpha).py

--- Spider_0(alpha).py
+++ Spider_0(alpha).py
@@ -4,26 +4,25 @@ from bs4 import BeautifulSoup
 
 
 
-def get_page():
+def get_page(url, headers):
+    req = urllib.request.Request(url, headers = headers)
+    response = urllib.request.urlopen(url).read()
+    html = response.decode('utf-8')
+    return html
+    
+def get_link(html):
     pass
 
 
 
-
-
-
-
 def main():
-    html = get_page()
+    url = 'mises.org'
+    headers = {
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063"
+    }
+    html = get_page(url, headers)
+    link = get_link(html)
     
-
-
-
-
-
-
-
-
 
 if '__name__' = '__main__':
     main()
